7aa558a09f62f4fd63c92856e36254e0ca89e182
Maximilian Severyn Wolfgang Esterhammer-Fic <52515120+mesterhammerfic@users.noreply.github.com>
2020-08-11 10:42:50 -0500

    Update ufcstats.py

--- src/ufcstats.py
+++ src/ufcstats.py
@@ -1,7 +1,130 @@
 import pandas as pd
 
+"""
+UFCStats.com Webscraping Functions
+
+These functions are used in the scapre scripts found in the /scraping/ folder of this repository
+"""
 
 
+"""
+===================================================================================
+Bout page functions
+
+Example of a bout page: http://www.ufcstats.com/fight-details/a2e34655a41ba45c
+===================================================================================
+"""
+def scrape_bout_page(bout_soup, bout_link):
+    #get event link
+    title = bout_soup.find(class_='b-content__title')
+    event_link = title.find('a').get('href')
+    #get details
+    details = parse_details(bout_soup)
+    #get rest of fight info
+    bouts = html_i_to_df(bout_soup, list_class="b-fight-details__text")
+    bouts['details'] = details
+    bouts['event_link'] = event_link
+    bouts['link'] = bout_link
+    return bouts
+
+def parse_details(bout_soup):    
+    """
+    input: beautiful soup of a ufcstats fight details page
+    output: a parsed string of the 'details' item
+    """
+    #get all detail items
+    details_elem = bout_soup.find_all(class_='b-fight-details__text')[1]
+    detail_items = details_elem.find_all('i', recursive=False)
+    #remove whitespace
+    detail_items = [item.get_text().split('\n') for item in detail_items]
+    details = []
+    for item in detail_items:
+        cleaned = [part.strip() for part in item]
+        details.append(''.join(cleaned))
+    details = ''.join(details)
+    #remove label
+    details = details[8:]
+    return details
+
+
+def parse_strikes(strikes_df, fighter_links, outcomes, bout_link):
+    """
+    input dataframe with striking bout info from ufcstats
+    """
+    strikes = format_stats_table(strikes_df)   
+    #add missing columns and clean up
+    strikes['bout_link'] = bout_link
+    strikes['outcome'] = outcomes
+    strikes = strikes.drop(labels = 'unnamed:_9_level_0', axis=1)
+    #split
+    strikes_0 = strikes.applymap(lambda x: split(0, x))
+    strikes_1 = strikes.applymap(lambda x: split(1, x))
+    strikes_0['fighter_link'] = fighter_links[0]
+    strikes_1['fighter_link'] = fighter_links[1]
+    
+    return strikes_0, strikes_1
+
+
+def parse_general(general_df, fighter_links, outcomes, bout_link):
+    """
+    input dataframe with general bout info from ufcstats
+    """
+    general = format_stats_table(general_df)
+    #add missing columns and clean up
+    general['bout_id'] = bout_link
+    general['outcome'] = outcomes
+    general.columns = ['fighter', 'kd', 'sig_str', 'sig_str_prcnt', 'total_str', 'td_count',
+                       'td_prcnt', 'sub_att', 'pass', 'rev', 'round', 'bout_id', 'outcome']
+    #split
+    general_0 = general.applymap(lambda x: split(0, x))
+    general_1 = general.applymap(lambda x: split(1, x))
+    general_0['fighter_link'] = fighter_links[0]
+    general_1['fighter_link'] = fighter_links[1]
+    
+    return general_0, general_1
+
+
+"""
+===================================================================================
+Event page functions
+
+Example of an event page: http://www.ufcstats.com/event-details/bda04c573563cc2e
+===================================================================================
+"""
+def event_scraper(event_soup, event_link):
+    """
+    input: beautiful soup object of the event page
+    output: dataframe with all relevent info
+    """
+    info = html_li_to_df(event_soup, 'b-list__box-list', is_nested=False)
+    info['name'] = event_soup.find(class_='b-content__title-highlight').get_text().strip()
+    info['link'] = event_link
+    return info
+
+
+"""
+===================================================================================
+Fighter page functions
+
+Fighter page example: http://www.ufcstats.com/fighter-details/d3df1add9d9a7efb
+===================================================================================
+"""
+def fighter_scraper(fighter_soup, fighter_link):
+    """
+    input: beautiful soup object of the fighter page
+    output: dataframe with all relevent info
+    """
+    info = html_li_to_df(fighter_soup, 'b-list__box-list', is_nested=False)
+    info['name'] = fighter_soup.find(class_='b-content__title-highlight').get_text().strip()
+    info['link'] = fighter_link
+    return info
+
+
+"""
+===================================================================================
+General functions
+===================================================================================
+"""
 def split(fighter_index, value):
     """
     use this in an apply map function to get seperate fighters from a ufcstats table. 
@@ -117,104 +240,3 @@ def html_li_to_df(soup, list_class, has_header=False, is_nested=True):
     list_df.columns = [(col.strip()).replace(' ', '') for col in list_df.columns]
     
     return list_df
-"""
-===================================================================================
-Bout page functions
-===================================================================================
-"""
-def scrape_bout_page(bout_soup, bout_link):
-    #get event link
-    title = bout_soup.find(class_='b-content__title')
-    event_link = title.find('a').get('href')
-    #get details
-    details = parse_details(bout_soup)
-    #get rest of fight info
-    bouts = html_i_to_df(bout_soup, list_class="b-fight-details__text")
-    bouts['details'] = details
-    bouts['event_link'] = event_link
-    bouts['link'] = bout_link
-    return bouts
-
-def parse_details(bout_soup):    
-    """
-    input: beautiful soup of a ufcstats fight details page
-    output: a parsed string of the 'details' item
-    """
-    #get all detail items
-    details_elem = bout_soup.find_all(class_='b-fight-details__text')[1]
-    detail_items = details_elem.find_all('i', recursive=False)
-    #remove whitespace
-    detail_items = [item.get_text().split('\n') for item in detail_items]
-    details = []
-    for item in detail_items:
-        cleaned = [part.strip() for part in item]
-        details.append(''.join(cleaned))
-    details = ''.join(details)
-    #remove label
-    details = details[8:]
-    return details
-
-
-def parse_strikes(strikes_df, fighter_links, outcomes, bout_link):
-    """
-    input dataframe with striking bout info from ufcstats
-    """
-    strikes = format_stats_table(strikes_df)   
-    #add missing columns and clean up
-    strikes['bout_link'] = bout_link
-    strikes['outcome'] = outcomes
-    strikes = strikes.drop(labels = 'unnamed:_9_level_0', axis=1)
-    #split
-    strikes_0 = strikes.applymap(lambda x: split(0, x))
-    strikes_1 = strikes.applymap(lambda x: split(1, x))
-    strikes_0['fighter_link'] = fighter_links[0]
-    strikes_1['fighter_link'] = fighter_links[1]
-    
-    return strikes_0, strikes_1
-
-
-def parse_general(general_df, fighter_links, outcomes, bout_link):
-    """
-    input dataframe with general bout info from ufcstats
-    """
-    general = format_stats_table(general_df)
-    #add missing columns and clean up
-    general['bout_id'] = bout_link
-    general['outcome'] = outcomes
-    general.columns = ['fighter', 'kd', 'sig_str', 'sig_str_prcnt', 'total_str', 'td_count',
-                       'td_prcnt', 'sub_att', 'pass', 'rev', 'round', 'bout_id', 'outcome']
-    #split
-    general_0 = general.applymap(lambda x: split(0, x))
-    general_1 = general.applymap(lambda x: split(1, x))
-    general_0['fighter_link'] = fighter_links[0]
-    general_1['fighter_link'] = fighter_links[1]
-    
-    return general_0, general_1
-"""
-===================================================================================
-Event page functions
-===================================================================================
-"""
-def event_scraper(event_soup, event_link):
-    """
-    input: beautiful soup object of the event page
-    output: dataframe with all relevent info
-    """
-    info = html_li_to_df(event_soup, 'b-list__box-list', is_nested=False)
-    info['name'] = event_soup.find(class_='b-content__title-highlight').get_text().strip()
-    info['link'] = event_link
-    return info
-"""
-===================================================================================
-Event page functions
-===================================================================================
-"""
-def fighter_scraper(fighter_soup, fighter_link):
-    """
-    input: beautiful soup object of the fighter page
-    output: dataframe with all relevent info
-    """
-    info = html_li_to_df(fighter_soup, 'b-list__box-list', is_nested=False)
-    info['name'] = fighter_soup.find(class_='b-content__title-highlight').get_text().strip()
-    info['link'] = fighter_link
-    return info
